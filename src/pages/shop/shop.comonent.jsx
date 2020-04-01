0592465e306b336f80def1a0ef5733eea6ca49b0
mbalazy <p@mbalazy.pl>
2020-04-01 19:25:14 +0200

    Revert "move all project files to client folder and make server.js"
    This reverts commt 7e957be45408d8ff84eb96fafa1f7e03f7edc606.

--- src/pages/shop/shop.comonent.jsx
+++ src/pages/shop/shop.comonent.jsx
@@ -6,7 +6,7 @@ import { connect } from 'react-redux';
 import { fetchCollectionsStart } from '../../redux/reducers/shop/shop.actions';
 
 import CollectionOverviewContainer from '../../components/collection/collection-overview/collection-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+import CollectionPageContainer from '../../pages/collection/collection.container';
 
 const ShopPage = ({ fetchCollectionsStart, match }) => {
   useEffect(() => {
